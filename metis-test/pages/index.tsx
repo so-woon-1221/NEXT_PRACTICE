840d7f78bdde887ba022e15993008024ff09413b
jhjeon-0to1 <jhjeon@0to1partners.com>
2021-07-07 18:34:54 +0900

    디자인 수정

--- metis-test/pages/index.tsx
+++ metis-test/pages/index.tsx
@@ -7,10 +7,10 @@ const index = () => (
       <title>METIS</title>
     </Head>
     <div className="w-full pt-4 md:pl-8 pl-4 pr-4 ">
-      <div className="overflow-y-auto border-b flex flex-col">
+      <div className="overflow-y-auto border-b flex flex-col md:px-20">
         <h1 className="font-extrabold text-3xl border-b-2 p-2 mb-4">Title</h1>
-        <div className="w-full pl-2 border h-36">sub menu</div>
-        <div className="flex-grow border-2">Detail</div>
+        <div className="w-full pl-2 h-36">sub menu</div>
+        <div className="flex-grow">Detail</div>
         <div className="h-96">test</div>
         <div className="h-96">test</div>
         <div className="h-96">test</div>
