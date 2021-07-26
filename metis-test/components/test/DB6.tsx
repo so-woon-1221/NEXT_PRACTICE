7ac610c9e43c6b65a8c7020ad7f795cbcd04414d
so-woon-1221 <janus7359@gmail.com>
2021-07-26 14:25:48 +0900

    채널별 분석 - 구매율 변화 / 판매순위

--- metis-test/components/test/DB6.tsx
+++ metis-test/components/test/DB6.tsx
@@ -1,13 +1,41 @@
 import React, { useEffect, useState } from "react";
 import * as d3 from "d3";
+import { mode } from "d3";
 import { Pie, Summer } from "../../types/summer";
 import ChannelPie from "../channel/ChannelPie";
+import ChannelBar from "../channel/ChannelBar";
+import CustomerTable from "../channel/CustomerTable";
+
+interface brand {
+  id: number;
+  name: string;
+  can: boolean;
+}
 
 const DB6: React.FC = () => {
   const [pieData, setPieData] = useState<Pie[]>([]);
+  const [barData, setBarData] = useState<
+    Map<string, Array<{ YM: string; data: number }>>
+  >(new Map());
+  const [brandList, setBrandList] = useState<brand[]>([]);
+  const [yearTotal, setYearTotal] = useState<Map<string, number>>(new Map());
+  const [customerMap, setCustomerMap] = useState<
+    Map<
+      string,
+      Array<{
+        model: string;
+        customerAmount: number;
+        amount: number;
+        buyAmount: number;
+        brand: string;
+      }>
+    >
+  >(new Map());
+
   useEffect(() => {
     // @ts-ignore
     d3.json("data.json").then((data: Array<Summer>) => {
+      //pie chart Data /////////////////////////////////////////////////////////////////////////////////////
       const pie = d3.group(data, (d) => d.SMTD_SHOP_NAME);
       const tempPie: Array<Pie> = [];
       const keys = pie.keys();
@@ -26,10 +54,99 @@ const DB6: React.FC = () => {
         data.percent = Math.round((data.data / total) * 100);
       }
       setPieData(tempPie.sort((a, b) => b.data - a.data));
+      //////////
+
+      // bar chart Data ///////////////////////////////////////////////////////////////////////////////////////
+      const tempBar: Map<
+        string,
+        Array<{ YM: string; data: number }>
+      > = new Map();
+      const brandList = Array.from(pie.keys());
+      setBrandList(
+        brandList.map((d, i) => {
+          return { id: i, name: d, can: false };
+        }),
+      );
+      // eslint-disable-next-line no-restricted-syntax
+      for (const brand of brandList) {
+        const newBar = d3.group(pie.get(brand)!, (d) => d.YM);
+        const keys = newBar.keys();
+        const tempData = [];
+        // @ts-ignore
+        // eslint-disable-next-line no-restricted-syntax
+        for (const year of keys) {
+          const data = newBar.get(year);
+          if (data) {
+            tempData.push({ YM: year, data: data.length });
+          }
+        }
+        tempBar.set(brand, tempData);
+      }
+      setBarData(tempBar);
+      const yearData = d3.group(data, (d) => d.YM);
+      const yearTotal = new Map();
+      // @ts-ignore
+      // eslint-disable-next-line no-restricted-syntax
+      for (const year of yearData.keys()) {
+        yearTotal.set(year, yearData.get(year)!.length);
+      }
+      setYearTotal(yearTotal);
+      //////////
+
+      // 채널별 구매자수 //////////////////////////////////////////////////////////////////////////////////////
+      const customerMap: Map<
+        string,
+        Array<{
+          model: string;
+          customerAmount: number;
+          amount: number;
+          buyAmount: number;
+          brand: string;
+        }>
+      > = new Map();
+      // eslint-disable-next-line no-restricted-syntax
+      for (const brand of brandList) {
+        const modelData = d3.group(pie.get(brand)!, (d) => d.MODEL_NAME_adj);
+        const keys = modelData.keys();
+        let tempData: Array<{
+          model: string;
+          customerAmount: number;
+          amount: number;
+          buyAmount: number;
+          brand: string;
+        }> = [];
+        //@ts-ignore
+        // eslint-disable-next-line no-restricted-syntax
+        for (const model of keys) {
+          let buyAmount = 0;
+          let amount = 0;
+          const customerAmount = modelData.get(model)!.length;
+          let brand = "";
+          for (let i = 0; i < customerAmount; i++) {
+            buyAmount += modelData.get(model)![i].QNTY_SUM;
+            amount += modelData.get(model)![i].AMT_SUM;
+            brand = modelData.get(model)![i].BRAND_NAME;
+          }
+          tempData.push({
+            model: model.replace(/(\s*)/g, ""),
+            customerAmount,
+            amount,
+            buyAmount,
+            brand,
+          });
+        }
+        tempData = tempData
+          .sort((a, b) => b.customerAmount - a.customerAmount)
+          .slice(0, 10);
+        customerMap.set(brand, tempData);
+      }
+      setCustomerMap(customerMap);
+      //////
     });
   }, []);
+
   return (
-    <div>
+    <div className="space-y-4">
       <div className="flex lg:space-x-4 flex-wrap lg:space-y-0 space-y-4">
         <div className="lg:w-half flex-grow bg-gray-200 rounded-xl h-96 w-full hover:shadow-xl relative">
           {pieData.length > 1 && (
@@ -39,8 +156,31 @@ const DB6: React.FC = () => {
             </>
           )}
         </div>
-        <div className="lg:w-half flex-grow bg-gray-200 rounded-xl h-96 w-full">
-          aa
+        <div className="lg:w-half flex-grow bg-gray-200 rounded-xl h-96 w-full relative hover:shadow-xl">
+          {barData.size > 1 && (
+            <>
+              <span className="absolute top-4 left-4">채널별 구매율 변화</span>
+              {/*@ts-ignore*/}
+              <ChannelBar
+                data={barData}
+                brandList={brandList}
+                yearTotal={yearTotal}
+              />
+            </>
+          )}
+        </div>
+      </div>
+      <div className="flex lg:space-x-4 flex-wrap lg:space-y-0 space-y-4">
+        <div className="lg:w-half flex-grow bg-gray-200 rounded-xl h-100 w-full hover:shadow-xl relative">
+          {customerMap.size > 1 && (
+            <>
+              <span className="absolute top-4 left-4">채널별 판매순위</span>
+              <CustomerTable data={customerMap} brandList={brandList} />
+            </>
+          )}
+        </div>
+        <div className="lg:w-half flex-grow bg-gray-200 rounded-xl h-100 w-full hover:shadow-xl relative">
+          <div>dd</div>
         </div>
       </div>
     </div>
